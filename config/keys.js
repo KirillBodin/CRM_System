946077cf7bc8137b5643b30beb8aba4ea6f37571
kiril <kirillbodin2@gmail.com>
2022-07-25 00:21:29 +0300

    Auth Bearer test

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports={
-    mongoUri:"mongodb+srv://kere:k5HrRtiNhvgnG8B@crmdb.dh0lfp5.mongodb.net/?retryWrites=true&w=majority"
+    mongoUri:"mongodb+srv://kere:k5HrRtiNhvgnG8B@crmdb.dh0lfp5.mongodb.net/?retryWrites=true&w=majority",
+    jwt: 'key-jwt'
 
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,36 @@
 const User=require("../models/User")
 const bcrypt=require("bcryptjs")
-module.exports.login = function(req, res) {
-  res.status(200).json({
-    login: {
-      email:req.body.email,
-      password:req.body.password
+const jwt=require("jsonwebtoken")
+const keys=require('../config/keys')
+
+
+module.exports.login = async function(req, res) {
+const candidate = await User.findOne({email: req.body.email})
+
+  if(candidate){
+    //check password>, user exist
+    const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+    if (passwordResult){
+      //generate token. passwords matches
+      const token = jwt.sign({
+        email:candidate.email,
+        userId: candidate._id
+      }, keys.jwt,{expiresIn: 60 * 60})
+
+      res.status(200).json({
+        token: `Bearer ${token}`
+      })
+    }else {
+      res.status(401).json({
+        massage:"Try again. Wrong password"
+      })
     }
-  })
+  }else {
+    // No User, err
+    res.status(404).json({
+      massage:"Can't find user with that email"
+    })
+  }
 }
 
 
